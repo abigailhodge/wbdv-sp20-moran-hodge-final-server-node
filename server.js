264250166f5d319c88563c952c0e14dd91d6888e
abigailhodge <abigailhodge98@gmail.com>
2020-03-26 01:11:14 -0400

    finished proxy server

--- server.js
+++ server.js
@@ -3,40 +3,7 @@ const express = require("express");
 
 const app = express();
 const client = new Client({});
-
+const placesController = require('./controllers/places.controller.server')
+placesController(app, client)
 app.listen(3000);
 
-app.get('/api/places', (req, res) => res.send('all places'))
-
-app.get('/api/places/:placeid', (req, res) => res.send('specific place'))
-
-// client
-//   .textSearch({
-//     params: {
-//       query: "coffee",
-//       key: process.env.GOOGLE_MAPS_API_KEY,
-//     },
-//     timeout: 1000 // milliseconds
-//   })
-//   .then(r => {
-//     console.log(r.data.results);
-//   })
-//   .catch(e => {
-//     console.log(e);
-//   });
-
-//   client
-//   .details({
-//     params: {
-//       place_id: "coffee",
-//       key: process.env.GOOGLE_MAPS_API_KEY,
-//       fields: "formatted_address,name,photo,vicinity,formatted_phone_number,opening_hours,website,price_level,rating,review"
-//     },
-//     timeout: 1000 // milliseconds
-//   })
-//   .then(r => {
-//     console.log(r.data.results);
-//   })
-//   .catch(e => {
-//     console.log(e);
-//   });
