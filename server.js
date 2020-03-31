43ee2a463c667d79670954de5b19e444750fb011
abigailhodge <abigailhodge98@gmail.com>
2020-03-31 12:07:08 -0400

    configured CORS

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const Client = require("@googlemaps/google-maps-services-js").Client;
 const express = require("express");
 
 const app = express();
-const client = new Client({});
-const placesController = require('./controllers/places.controller.server')
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin",
+        "https://prototype-frontend.herokuapp.com/");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+ }); 
+ const placesController = require('./controllers/places.controller.server')
+ const client = new Client({});
 placesController(app, client)
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 5000);
 
